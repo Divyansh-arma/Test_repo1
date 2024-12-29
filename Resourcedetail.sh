0935dbe861d0b5d69d23ee2042b63da7f2b8c444
EC2 Default User <ec2-user@ip-172-31-21-66.ec2.internal>
2024-12-29 18:00:28 +0000

    Raw File

---
+++ Resourcedetail.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+########## This will show the resouce details in AWS ######
+
+# set -x 
+
+echo "List EC2 instances"
+
+
+aws ec2 describe-instances | jq '.Resarvation[].'
+
+
+2nd change
